33d33e1fb7d7f3bd9a70bd9bada47b18475bca87
K Balaji Naik <101827986+Balaji91221@users.noreply.github.com>
2023-11-16 16:15:51 +0000

    hi

--- src/pages/HomeCon.jsx
+++ src/pages/HomeCon.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import HomePage from '../components/Home/Home'
 import Dir from '../components/Home/dir'
-import Galleryimages from '../components/Home/Galleryimages'
+
 import Accordion from '../components/Home/DefaultAccordion'
 function HomeCon() {
   return (
     <div>
    <HomePage/>
    <Dir/>
-   <Galleryimages />
+   
    <Accordion className="bg-gray-50" />
 
     </div>
